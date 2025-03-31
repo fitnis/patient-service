f59a9c3ba9892c0b0da1f4cf4dd5b24ca796f278
Jakub Hrncar <jakubko.hrncar@gmail.com>
2025-03-31 13:29:23 +0200

    Init

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/fitnis/patient-service/configs"
-	"github.com/fitnis/patient-service/internal/database"
-	"github.com/fitnis/patient-service/internal/routes"
-	"github.com/labstack/echo/v4"
+	"github.com/fitnis/patient-service/handlers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Load configuration
-	config, err := configs.LoadConfig()
-	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+	router := gin.Default()
+
+	p := router.Group("/patients")
+	{
+		p.POST("/admit", handlers.AdmitPatient)
+		p.GET("/admit", handlers.GetPatients)
+		p.DELETE("/admit/:patientId", handlers.DischargePatient)
+		p.POST("/register", handlers.RegisterPatient)
 	}
 
-	// Connect to database
-	if err := database.ConnectDB(config); err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-
-	// Create a new Echo instance
-	e := echo.New()
-
-	// Setup routes
-	routes.SetupRoutes(e, config)
-
-	// Start server
-	log.Printf("Starting server on port %d", config.Port)
-	if err := e.Start(fmt.Sprintf(":%d", config.Port)); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	router.Run(":8082")
 }
